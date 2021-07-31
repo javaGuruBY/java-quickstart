3cc2a989d480a2af2ff075665bbf29bb4f1e1059
Maksim Shelkovich <info@jrr.by>
2021-07-31 16:51:52 +0300

    adds jar with dependencies package

--- src/main/java/com/tutrit/java/quickstart/Application.java
+++ src/main/java/com/tutrit/java/quickstart/Application.java
@@ -1,9 +1,13 @@
 package com.tutrit.java.quickstart;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application {
+    private static Logger log = LoggerFactory.getLogger("main");
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        log.info("Hello world!");
     }
 }
 
